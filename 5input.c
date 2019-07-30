c09821919e2d5b0fe96b852dffc81e03993fbfda
SaurabhKolhe <50330087+SaurabhKolhe@users.noreply.github.com>
2019-07-30 21:55:35 +0530

    Create 5input.c

---
+++ 5input.c
@@ -0,0 +1,14 @@
+/*
+In C we can take input from user.
+scanf() function is to store value;
+scanf() contain to parameter i.e. format specifier and address of variable i.e. &variable_name.
+*/
+
+#include<stdio.h>
+void main()
+{
+int a;
+printf("Give any input to store");
+scanf("%d",&a);
+print("%d",a);
+}
