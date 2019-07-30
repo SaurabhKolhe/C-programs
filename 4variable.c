a473be4aaad0e75ff646a5ef3270b059cacea96a
SaurabhKolhe <50330087+SaurabhKolhe@users.noreply.github.com>
2019-07-30 21:38:16 +0530

    Create 4variable.c

---
+++ 4variable.c
@@ -0,0 +1,14 @@
+/*Variable : Naming of address is Variable
+             Variable is come with its datatype which decide type of data will store in address.
+             Variable not start with number or any symbol.It start with _ or alphabet
+Syntax     : Datatype Varaiable_name = Variables_value;
+        eg : int a = 20;
+*/
+
+#include<stdio.h>
+void main()
+{
+int a;        //variable declaration.
+a=20;         //variable defination.
+printf("In this 'a' is variable having value %d",a);
+}
