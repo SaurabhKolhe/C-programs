17a441462ee1de36915bca16a962c6e881b918df
SaurabhKolhe <50330087+SaurabhKolhe@users.noreply.github.com>
2019-07-30 20:00:57 +0530

    Update comment.c

--- comment.c
+++ comment.c
@@ -1,12 +1,12 @@
 /*There are 2 types of comment in C
 1.Single Line   -  // 
 2.Multi Line    -  /* comment  */
-*/
+
 #include <stdio.h>
 void main()
 {
 //Comment is ont affecting on program.It is use to give information about program.
-print("HELLO");  //This line print HELLO on screen.
+printf("HELLO");  //This line print HELLO on screen.
 /*This is multi line comment
 In which we can write multiple lines in between.
 */
